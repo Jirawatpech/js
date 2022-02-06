21b555071b02dc2a12c7df9d86eff21a5f332d65
Jirawatpech <Jirawat_pech@gmail.com>
2022-02-06 21:10:40 +0700

    new

--- alert/alert3.js
+++ alert/alert3.js
@@ -1,10 +1,13 @@
-var x = document.getElementById("h1")
+
+
+  var x = document.getElementById("h1")
 x.addEventListener('click',showinformation.bind(event,x),false);
 var y = document.getElementById("p1");
 y.addEventListener('click',showinformation.bind(event,y),false);
 var z = document.getElementById("p2");
 z.addEventListener('click',showinformation.bind(event,z),false);
-
+var g = document.getElementById("h2")
+g.addEventListener('click',showinformation(event,g),true)
 function showinformation(a,ele)
 {
   alert(a.innerHTML);
